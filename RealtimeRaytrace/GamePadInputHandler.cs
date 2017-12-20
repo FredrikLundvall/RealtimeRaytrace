73d282b35be77894079c53b6ef27ab16bcfdc48c
Fredrik <lundvall_fredrik@hotmail.com>
2017-12-20 12:33:51 +0100

    The camera cant't be moved anymore

--- RealtimeRaytrace/GamePadInputHandler.cs
+++ RealtimeRaytrace/GamePadInputHandler.cs
@@ -19,16 +19,16 @@ namespace RealtimeRaytrace
 
         public void HandleInput(Queue<IPlayerCommand> playerCommandQueue)
         {
-            GamePadState gamepadState = GamePad.GetState(_playerIndex);
-            float speedStep = 1.0f;
-            if (gamepadState.Buttons.LeftStick == ButtonState.Pressed)
-                speedStep = 1.5f;
+            //GamePadState gamepadState = GamePad.GetState(_playerIndex);
+            //float speedStep = 1.0f;
+            //if (gamepadState.Buttons.LeftStick == ButtonState.Pressed)
+            //    speedStep = 1.5f;
 
-            playerCommandQueue.Enqueue(new MoveDepthCommand(gamepadState.ThumbSticks.Left.Y * -speedStep));
-            playerCommandQueue.Enqueue(new MoveSideCommand(gamepadState.ThumbSticks.Left.X * speedStep));
-            //playerCommandQueue.Enqueue(new MoveHeightCommand(upDistance));
-            playerCommandQueue.Enqueue(new RotateYawCommand(gamepadState.ThumbSticks.Right.X * -0.2f));
-            playerCommandQueue.Enqueue(new RotatePitchCommand(gamepadState.ThumbSticks.Right.Y * 0.2f));
+            //playerCommandQueue.Enqueue(new MoveDepthCommand(gamepadState.ThumbSticks.Left.Y * -speedStep));
+            //playerCommandQueue.Enqueue(new MoveSideCommand(gamepadState.ThumbSticks.Left.X * speedStep));
+            ////playerCommandQueue.Enqueue(new MoveHeightCommand(upDistance));
+            //playerCommandQueue.Enqueue(new RotateYawCommand(gamepadState.ThumbSticks.Right.X * -0.2f));
+            //playerCommandQueue.Enqueue(new RotatePitchCommand(gamepadState.ThumbSticks.Right.Y * 0.2f));
             if (GamePad.GetState(0).IsButtonDown(Buttons.Start))
                 playerCommandQueue.Enqueue(new QuitCommand());
         }
